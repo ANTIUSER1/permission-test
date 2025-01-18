d704e0da411854df4a6a98051377f59a9d0805bc
PNS <solovjov_pn@mail.ru>
2025-01-18 10:17:38 +0300

    0+0

---
+++ bagira/src/test/java/pn/cp/bagira/BagiraApplicationTests.java
@@ -0,0 +1,13 @@
+package pn.cp.bagira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
